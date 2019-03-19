573035c4c2243f6017e3de9de92978121322c13e
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-19 21:44:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
-  array = string.split(" ")
   new_array = []
-  array.each do |word|
+  array = string.split(" ")
+   array.each do |word|
     new_array << word.reverse
-  end
+   end
   new_array.join(" ")
 end
 
